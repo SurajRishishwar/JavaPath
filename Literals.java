22ca2d798a2919725117fb2e06a29abf1119be09
Suraj Sharma <rishishwarsuraj@gmail.com>
2023-09-22 22:24:19 +0530

    Literals in Java

---
+++ Literals.java
@@ -0,0 +1,14 @@
+class Literals{
+    public static void main(String a[]){
+        // literals means the value which we assign.
+
+        // here both values are same but both are different literals.
+        int n = 5; 
+        int n1 = 0b101;
+        int n2 = 0x4A;
+
+
+        System.out.println(n);
+        System.out.println(n2);
+    }
+}
