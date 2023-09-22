9da2722019225ea8279cd4a8cca93ce174e47cd8
Suraj Sharma <rishishwarsuraj@gmail.com>
2023-09-22 22:19:04 +0530

    Data Types & their initialization

---
+++ DataTypes.java
@@ -0,0 +1,17 @@
+class DataTypes{
+    public static void main(String a[]){
+        byte b = 125;
+        short s = 453;
+        int n = 544;
+        long l = 45678l; // followed by l in the end.
+
+        float f = 78.3f; // followed by f in the end.
+        double d = 333.9033;
+
+        char c = 'k'; // we can not use " ".
+
+        boolean bb = true; // we can not use 0 and 1 inplace of true and false.
+
+        System.out.println(bb);
+    }
+}
