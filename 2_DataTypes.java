ff2bdfd77604563da8ead4cccccdef162408951a
Suraj Sharma <rishishwarsuraj@gmail.com>
2023-09-22 22:38:57 +0530

    Data Types

--- 2_DataTypes.java
+++ 2_DataTypes.java
@@ -2,13 +2,13 @@ class DataTypes{
     public static void main(String a[]){
         byte b = 125;
         short s = 453;
-        int n = 544;
+        int n = 5456;
         long l = 45678l; // followed by l in the end.
 
         float f = 78.3f; // followed by f in the end.
-        double d = 333.9033;
+        double d = 233.9033;
 
-        char c = 'k'; // we can not use " ".
+        char c = 's'; // we can not use " ".
 
         boolean bb = true; // we can not use 0 and 1 inplace of true and false.
 
