d877b1af0cf711fb2510d45a4b9765c3c6821fc3
Suraj Sharma <rishishwarsuraj@gmail.com>
2023-09-23 23:21:20 +0530

    if-else

---
+++ 8_Conditional.java
@@ -0,0 +1,41 @@
+class Conditional{
+    public static void main(String ar[]){
+        // use case 1:-
+        if(/*Condition*/true){
+
+            //statement
+
+        }
+
+
+        // use case 2:-
+        if(/*Condition*/true){
+            
+            //statement
+        
+        }else{
+            
+            //statement
+        
+        }
+
+
+        // use case 3:-
+        if(/*Condition 1*/false){
+            
+            //statement
+        
+        }else if(/*Condition 2*/true){
+            
+            //statement
+        
+        }else if(/*Condition 3*/false){
+            
+            //statement
+        
+        }else{
+            
+            //statement
+        }
+    }
+}
