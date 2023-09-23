943217a328168e688e12502fa25dc5453edd5339
Suraj Sharma <rishishwarsuraj@gmail.com>
2023-09-23 23:12:30 +0530

    Logical Operator

---
+++ 7_LogicalOperator.java
@@ -0,0 +1,9 @@
+class Logical{
+    public static void main(String ar[]){
+        System.out.println(true && true); // and = &&, or = ||, not = !
+        System.out.println(true || false);
+    }
+}
+// in and if both conditions are true then is to be true.
+// in or if any one of condition is true then is to be true.
+// in not is flipped the condition[false to true]or vice versa.
