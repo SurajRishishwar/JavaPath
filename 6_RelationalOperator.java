9d8090015613fa310fbb85ea8e4faec8ae381942
Suraj Sharma <rishishwarsuraj@gmail.com>
2023-09-23 23:07:12 +0530

    Relational Operator

---
+++ 6_RelationalOperator.java
@@ -0,0 +1,8 @@
+class Relational{
+    public static void main(String ar[]){
+
+        int x = 45;
+        int y = 555;
+        System.out.println(x > y);  // >, <, >=, <=, !=. == operators gives True or False. 
+    }
+}
