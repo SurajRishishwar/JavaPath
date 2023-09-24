3ebf839863503d5d105d6f2e6993a40018e27f0f
Suraj Sharma <rishishwarsuraj@gmail.com>
2023-09-24 18:04:28 +0530

    Method Overriders

---
+++ A4_MethodOverriding.java
@@ -0,0 +1,15 @@
+class Overriders{
+    public static void main(String ar[]){
+        Container cont = new Container(); 
+        int r = cont.add(34,7,9);
+        System.out.println(r);
+    }
+}
+class Container{
+    public int add(int a,int b){
+        return a+b;
+    }
+    public int add(int a,int b,int c){
+        return a+b+c;
+    }
+}
