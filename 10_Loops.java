6226350f918393884b0fed77d02c55b8297c7a9b
Suraj Sharma <rishishwarsuraj@gmail.com>
2023-09-24 13:30:38 +0530

    Loops

---
+++ 10_Loops.java
@@ -0,0 +1,25 @@
+class Loops{
+    public static void main(String ar[]){
+    
+        // While Loop
+        int i=1;
+        while(/*condition*/i <= 4){
+            System.out.println("Its While");
+            i++;
+        }
+
+        // for Loop
+        for(int j=0;j<4;j++){
+            System.out.println("For Loop");
+        }
+
+        // Do While
+        int k=1;
+        do{
+            System.out.println("Do While");
+            k++;
+        }while(k<5);
+
+
+    }
+}
