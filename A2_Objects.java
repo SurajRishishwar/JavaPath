f5b96cf14c2964e6f2fc41aa48878f7c2294d424
Suraj Sharma <rishishwarsuraj@gmail.com>
2023-09-24 17:43:22 +0530

    Classes & Objects

--- A2_Objects.java
+++ A2_Objects.java
@@ -1,6 +1,6 @@
 class Objects{
     public static void main(String ar[]){
-        int number1=8;
+        int number1=88;
         int number2=11;
         Calculator calc = new Calculator();
         int res = calc.multiplication(number1,number2);
